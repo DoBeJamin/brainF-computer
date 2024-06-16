e4d0352210f59f93c0731000fb079a8e63b3a6f5
DoBeJamin <83788679+DoBeJamin@users.noreply.github.com>
2024-06-15 22:39:39 -0500

    version 1

---
+++ brainf to macchine code.py
@@ -0,0 +1,78 @@
+# brainf Code examples
+#brainfCode = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+#brainfCode = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+brainfCode = "+++++++++++>+>>>>++++++++++++++++++++++++++++++++++++++++++++>++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-<-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<-]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<+>>[-]]<<<<<<<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++++++++++++++++++++++++++++++++++++++++++++++.[-]<<<<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<[-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]"
+
+
+loopStarts = []
+loopStartCodes = {}
+loopEndCodes = {}
+
+#loop code generation
+for idx, character in enumerate(brainfCode):
+
+    if character == "[":
+        loopStarts.append(idx)
+        
+        continue
+    elif character == "]":
+        openBracketLocation = loopStarts.pop()
+
+
+        #start bracket codes
+        endBracketLocationHex = hex(idx+1)[2:]
+
+        startBracketCode = ""
+        for _ in range(4-len(endBracketLocationHex)):
+            startBracketCode += "0"
+        startBracketCode += endBracketLocationHex
+        startBracketCode += "0"
+
+        loopStartCodes[openBracketLocation] = startBracketCode 
+
+
+        #end bracket codes
+        startBracketLocationHex = hex(openBracketLocation)[2:]
+
+        endBracketCode = ""
+        for _ in range(4-len(startBracketLocationHex)):
+            endBracketCode += "0"
+        endBracketCode += startBracketLocationHex
+        endBracketCode += "1"
+
+        loopEndCodes[idx] = endBracketCode
+
+
+machineCode = """"""
+
+#machine code generation
+for idx, character in enumerate(brainfCode):
+    if character == "+":
+        machineCode += "44444"
+    elif character == "-":
+        machineCode += "55555"
+    elif character == ">":
+        machineCode += "22222"
+    elif character == "<":
+        machineCode += "33333"
+    elif character == ".":
+        machineCode += "66666"
+    elif character == ",":
+        machineCode += "77777"        
+    elif character == "[":
+        machineCode += loopStartCodes[idx]
+        #print(loopStartCodes[idx])
+    elif character == "]":
+        machineCode += loopEndCodes[idx]
+        #print(loopEndCodes[idx])
+    machineCode += "\n"
+    
+print(machineCode)
+
+
+
+
+
+
+
+
